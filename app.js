27abbc0c6dbac05cd5c33a69e3f7795cb8c12e0b
Zakir Trimatric <zakirtrimatric@gmail.com>
2022-10-26 17:21:07 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+//access data from various location
+app.use(cors());
+//for access body form data
+app.use(bodyParser.urlencoded({extended:true}));
+//for use json data
+app.use(bodyParser.json());
+//home Route
+app.get('/',(req, res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+})
+//bad request route
+app.use((req, res, next)=>{
+    res.status(404).json({
+        message: 'Route not found',
+    })
+});
+//server error
+app.use((err,req, res, next)=>{
+    res.status(500).json({
+        message: 'something Broke',
+    })
+});
+
+module.exports = app;
